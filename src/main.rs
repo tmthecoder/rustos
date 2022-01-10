cd1df921563eb98327cb1d6c92b7e744d9febf5e
Tejas Mehta <tmthecoder@gmail.com>
2022-01-10 02:54:46 -0500

    add the vga buffer display on boot!

--- src/main.rs
+++ src/main.rs
@@ -3,9 +3,24 @@
 
 use core::panic::PanicInfo;
 
+// Hello world byt to bytes
+static HELLO: &[u8] = b"Hello World!";
+
+// Entry function as the linker looks for '_start()' by default
 #[no_mangle] // Don't mangle this as it's the entrypoint
 pub extern "C" fn _start() -> ! {
-    // Entry function as the linker looks for '_start()' by default
+    // The location of the vga buffer
+    let vga_buffer = 0xb8000 as *mut u8;
+
+    for (i, &byte) in HELLO.iter().enumerate() {
+        unsafe {
+            // Write the string byte
+            *vga_buffer.offset(i as isize * 2) = byte;
+            // Write the color byte (0xb is light cyan)
+            *vga_buffer.offset(i as isize * 2 + 1) = 0xb
+        }
+    }
+
     loop{}
 }
 
