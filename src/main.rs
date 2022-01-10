a436b9fe2b6ea0020071ae5b48e7059f129d61e1
Tejas Mehta <tmthecoder@gmail.com>
2022-01-10 03:40:57 -0500

    Create an abstration for the VGA buffer to reduce the use of unsafe blocks and easily print strings

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,10 @@
 #![no_std]
 #![no_main]
 
+use core::fmt::Write;
 use core::panic::PanicInfo;
+
+mod vga_buffer;
 
 // Hello world byt to bytes
 static HELLO: &[u8] = b"Hello World!";
@@ -9,18 +12,10 @@ static HELLO: &[u8] = b"Hello World!";
 // Entry function as the linker looks for '_start()' by default
 #[no_mangle] // Don't mangle this as it's the entrypoint
 pub extern "C" fn _start() -> ! {
-    // The location of the vga buffer
-    let vga_buffer = 0xb8000 as *mut u8;
-
-    for (i, &byte) in HELLO.iter().enumerate() {
-        unsafe {
-            // Write the string byte
-            *vga_buffer.offset(i as isize * 2) = byte;
-            // Write the color byte (0xb is light cyan)
-            *vga_buffer.offset(i as isize * 2 + 1) = 0xb
-        }
-    }
-
+    use core::fmt::Write;
+    vga_buffer::WRITER.lock().write_str("Hello again").unwrap();
+    write!(vga_buffer::WRITER.lock(), ", some numbers: {} {}", 42, 1.337).unwrap();
+    vga_buffer::WRITER.lock().write_str("\n New Line Hello").unwrap();
     loop{}
 }
 
---
+++ src/vga_buffer.rs
@@ -0,0 +1,151 @@
+#[allow(dead_code)]
+#[derive(Debug, Clone, Copy, PartialEq, Eq)]
+#[repr(u8)] // enum's underlying representation will be of type 'u8'
+// Enum to abstract colors allowed by the VGA buffer
+pub enum Color {
+    Black = 0,
+    Blue = 1,
+    Green = 2,
+    Cyan = 3,
+    Red = 4,
+    Magenta = 5,
+    Brown = 6,
+    LightGray = 7,
+    DarkGray = 8,
+    LightBlue = 9,
+    LightGreen = 10,
+    LightCyan = 11,
+    LightRed = 12,
+    Pink = 13,
+    Yellow = 14,
+    White = 15
+}
+
+#[derive(Debug, Clone, Copy, PartialEq, Eq)]
+#[repr(transparent)] // Ensures ColorCode has the same layout as a u8
+struct ColorCode(u8); // Struct to represent a color code
+
+impl ColorCode {
+    fn new(foreground: Color, background: Color) -> ColorCode {
+        // Color code contains the full color byte (including foreground and background)
+        ColorCode((background as u8) << 4 | (foreground as u8))
+    }
+}
+
+#[derive(Debug, Clone, Copy, PartialEq, Eq)]
+#[repr(C)] // Guarantees that struct is laid out like a C struct
+struct ScreenChar {
+    ascii_character: u8,
+    color_code: ColorCode
+}
+
+// The writable screen height (rows in the 2D array)
+const BUFFER_HEIGHT: usize = 25;
+// The writable screen width (columns in the 2D array)
+const BUFFER_WIDTH: usize = 80;
+
+use volatile::Volatile;
+#[repr(transparent)] // Ensures the Buffer has the same layout as its single field
+// The abstracted representation of the VGA Buffer
+struct Buffer {
+    chars: [[Volatile<ScreenChar>; BUFFER_WIDTH]; BUFFER_HEIGHT],
+}
+
+pub struct Writer {
+    column_position: usize, // Current position in the last row
+    color_code: ColorCode, // Color code for current color
+    buffer: &'static mut Buffer, // Reference to the VGA buffer
+}
+
+impl Writer {
+    // The method that writes a single byte to the VGA buffer, with the given color code
+    pub fn write_byte(&mut self, byte: u8) {
+        match byte {
+            b'\n' => self.new_line(), // If the byte is a newline byte, just call the newline method
+            byte => {
+                if self.column_position >= BUFFER_WIDTH {
+                    // Go to the next line if the current one is full
+                    self.new_line();
+                }
+
+                // Get the row and column of the desired write
+                let row = BUFFER_HEIGHT - 1;
+                let col = self.column_position;
+
+                // Get the color code
+                let color_code = self.color_code;
+                // Write the new 'ScreenChar' to the buffer at the current position
+                self.buffer.chars[row][col].write(ScreenChar {
+                    ascii_character: byte,
+                    color_code
+                });
+                self.column_position += 1;
+            }
+        }
+    }
+
+    // Moves each character one line up (deleting the top if applicable) and starts at tge beginning of the last line again
+    fn new_line(&mut self) {
+        // Loop through each row (except the top) and column, moving all existing characters one row up
+        for row in 1..BUFFER_HEIGHT {
+            for col in 0..BUFFER_WIDTH {
+                let character = self.buffer.chars[row][col].read();
+                self.buffer.chars[row - 1][col].write(character)
+            }
+        }
+        // Clear the bottom row
+        self.clear_row(BUFFER_HEIGHT - 1);
+        // Reset column position
+        self.column_position = 0;
+    }
+
+    fn clear_row(&mut self, row: usize) {
+        // The blank character (just a space)
+        let blank = ScreenChar {
+            ascii_character: b' ',
+            color_code: self.color_code,
+        };
+        // Set each item in the buffer to the blank character
+        for col in 0..BUFFER_WIDTH {
+            self.buffer.chars[row][col].write(blank)
+        }
+    }
+}
+
+impl Writer {
+    // A convenience method to write an entire string to the VGA buffer
+    pub fn write_string(&mut self, s: &str) {
+        for byte in s.bytes() {
+            match byte {
+                // Check if byte is in printable ASCII range (or newline)
+                0x20..=0x7e | b'\n' => self.write_byte(byte),
+                // Byte isn't in printable ASCII range
+                _ => self.write_byte(0xfe)
+            }
+        }
+    }
+}
+
+use core::fmt; // Allows access to the fmt::Write trait
+// Implement write! format macros for the Writer struct
+impl fmt::Write for Writer {
+    fn write_str(&mut self, s: &str) -> fmt::Result {
+        self.write_string(s);
+        Ok(())
+    }
+}
+
+use lazy_static::lazy_static;
+use spin::Mutex;
+// The global interface to use as a writer from external code
+// Needs 'lazy_static' as you can't convert raw pointers to references at compile time
+lazy_static! {
+    pub static ref WRITER: Mutex<Writer> = Mutex::new(Writer {
+        column_position: 0,
+        color_code: ColorCode::new(Color::Blue, Color::Black),
+        // The location of the vga buffer: 0xb8000
+        // VGA Buffer article: https://os.phil-opp.com/vga-text-mode/
+        buffer: unsafe { &mut *(0xb8000 as *mut Buffer) },
+    });
+}
+
