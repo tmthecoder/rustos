eb40c0891c7daee81279e8e3f0c401e1aa9eb507
Tejas Mehta <tmthecoder@gmail.com>
2022-01-13 18:49:04 -0500

    create a custom frame allocator and mapper function to test it

--- src/main.rs
+++ src/main.rs
@@ -8,37 +8,46 @@ use core::panic::PanicInfo;
 use bootloader::{BootInfo, entry_point};
 use x86_64::structures::paging::Translate;
 use rustos::{memory, println};
+use rustos::memory::BootInfoFrameAllocator;
 
 // The bootloader package's provided macro to set the entry point of the OS
 entry_point!(kernel_main);
 
 // Rust type-checked entry function with the 'boot_info' parameter
 fn kernel_main(boot_info: &'static BootInfo) -> ! {
-    use x86_64::VirtAddr;
+    use x86_64::{structures::paging::Page, VirtAddr};
 
     println!("Hello World{}", "!");
     rustos::init();
 
     let phys_mem_offset = VirtAddr::new(boot_info.physical_memory_offset);
-    let mapper = unsafe { memory::init(phys_mem_offset) };
+    let mut mapper = unsafe { memory::init(phys_mem_offset) };
+    let mut frame_allocator = unsafe { BootInfoFrameAllocator::init(&boot_info.memory_map) };
 
+    // Map an unused page
+    let page = Page::containing_address(VirtAddr::new(0xdeadbeaf000));
+    memory::create_example_mapping(page, &mut mapper, &mut frame_allocator);
 
-    let addresses = [
-        // the identity-mapped vga buffer page
-        0xb8000,
-        // some code page
-        0x201008,
-        // some stack page
-        0x0100_0020_1a10,
-        // cirtual address mapped to physical address 0
-        boot_info.physical_memory_offset
-    ];
+    // Write the string 'New!' to the screen through the new mapping
+    let page_ptr: *mut u64 = page.start_address().as_mut_ptr();
+    unsafe { page_ptr.offset(400).write_volatile(0x_f021_f077_f065_f04e) };
 
-    for &address in &addresses {
-        let virt = VirtAddr::new(address);
-        let phys = mapper.translate_addr(virt);
-        println!("{:?} -> {:?}", virt, phys);
-    }
+    // let addresses = [
+    //     // the identity-mapped vga buffer page
+    //     0xb8000,
+    //     // some code page
+    //     0x201008,
+    //     // some stack page
+    //     0x0100_0020_1a10,
+    //     // cirtual address mapped to physical address 0
+    //     boot_info.physical_memory_offset
+    // ];
+    //
+    // for &address in &addresses {
+    //     let virt = VirtAddr::new(address);
+    //     let phys = mapper.translate_addr(virt);
+    //     println!("{:?} -> {:?}", virt, phys);
+    // }
 
     #[cfg(test)]
     test_main(); // Call that renamed function on testing configs
--- src/memory.rs
+++ src/memory.rs
@@ -1,9 +1,83 @@
-use x86_64::{PhysAddr, structures::paging::PageTable, VirtAddr};
+use bootloader::bootinfo::{MemoryMap, MemoryRegionType};
+use x86_64::{
+    PhysAddr,
+    structures::paging::{PageTable, Page, PhysFrame, Mapper, Size4KiB, FrameAllocator},
+    VirtAddr
+};
 use x86_64::structures::paging::OffsetPageTable;
+
+/// A FrameAllocator that returns usable frames from the bootloader's memory map
+pub struct BootInfoFrameAllocator {
+    memory_map: &'static MemoryMap,
+    next: usize,
+}
+
+impl BootInfoFrameAllocator {
+    /// A convenience method to create a FrameAllocator from the passed memory map
+    ///
+    /// Unsafe as the caller must guarantee that the passed map is valid
+    /// To be valid, all frames marked 'USABLE' must truly be usable
+    pub unsafe fn init(memory_map: &'static MemoryMap) -> Self {
+        BootInfoFrameAllocator {
+            memory_map,
+            next: 0
+        }
+    }
+
+    /// Returns an iterator over the usable frames specified in the memory map
+    fn usable_frames(&self) -> impl Iterator<Item = PhysFrame> {
+        let regions = self.memory_map.iter();
+        // Get the usable regions from the memory map
+        let usable_regions = regions.filter(|r| r.region_type == MemoryRegionType::Usable);
+        // Get a list of address ranges for each region
+        let addr_ranges = usable_regions.map(|r| r.range.start_addr()..r.range.end_addr());
+        // Change ranges to a list of frame start addresses
+        let frame_addresses = addr_ranges.flat_map(|r| r.step_by(4096));
+        // Create a 'PhysFrame' type from each frame start address
+        frame_addresses.map(|addr| PhysFrame::containing_address(PhysAddr::new(addr)))
+    }
+}
+
+unsafe impl FrameAllocator<Size4KiB> for BootInfoFrameAllocator {
+    fn allocate_frame(&mut self) -> Option<PhysFrame> {
+        // Get the next needed frame
+        let frame = self.usable_frames().nth(self.next);
+        // Incrememnt the next counter
+        self.next += 1;
+        frame
+    }
+}
+
+/// A FrameAllocator that always returns 'None'
+pub struct EmptyFrameAllocator;
+
+unsafe impl FrameAllocator<Size4KiB> for EmptyFrameAllocator {
+    fn allocate_frame(&mut self) -> Option<PhysFrame<Size4KiB>> {
+       None
+    }
+}
 
 pub unsafe fn init(physical_memory_offset: VirtAddr) -> OffsetPageTable<'static> {
     let level_4_table = active_level_4_table(physical_memory_offset);
     OffsetPageTable::new(level_4_table, physical_memory_offset)
+}
+
+// Creates an example mapping for the given page to frane '0xb8000'
+pub fn create_example_mapping(
+    page: Page,
+    mapper: &mut OffsetPageTable,
+    frame_allocator: &mut dyn FrameAllocator<Size4KiB>,
+) {
+    use x86_64::structures::paging::PageTableFlags as Flags;
+
+    let frame = PhysFrame::containing_address(PhysAddr::new(0xb8000));
+    let flags = Flags::PRESENT | Flags::WRITABLE;
+
+    let map_to_result = unsafe {
+        // FIXME: this isn't safe, just an example
+        mapper.map_to(page, frame, flags, frame_allocator)
+    };
+    map_to_result.expect("map_to failed").flush();
 }
 
 // A function to retrieve a mutable reference to the active level 4 page table
